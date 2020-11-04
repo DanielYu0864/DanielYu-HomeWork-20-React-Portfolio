4352d797cee6b5b8d4530b531b2ec7f1dbe0ea71
Daniel.Yu <adam741963@gmail.com>
2020-11-04 14:32:44 -0800

    contact setup

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -3,7 +3,13 @@ import React from 'react'
 function Contact() {
   return (
     <div>
-      Contact
+      <ul>
+        Contact Me:
+          <p>Phone: <a href="tel:+14256254073">425-625-4073</a></p>
+          <p>Email: <a href="mailto:adam741963@gmail.com">adam741963@gmail.com</a></p>
+          <p>Linkedin: <a href="https://www.linkedin.com/in/daniel-yu-264360199/">Daniel Yu</a></p>
+          <p>Github: <a href="https://github.com/DanielYu0864">DanielYu0864</a></p>
+      </ul>
     </div>
   )
 }
