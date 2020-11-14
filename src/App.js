f698e0f8db4125a414338a0231bc3420dbf84e95
Daniel.Yu <adam741963@gmail.com>
2020-11-14 15:00:15 -0800

    fixed react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header';
 import Nav from './Components/Nav';
@@ -7,6 +7,22 @@ import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Intro from './pages/Intro';
+import Media from './Components/Media';
+// const Intro = () => {
+//   <div className='intro'>
+//   <div className='hp'>
+//     <h1>
+//       I am Daniel A. Yu
+//     </h1>
+//     <p>
+//       a lifelong learner of knowledge and current Boot Camp student who presently has sight set on a fullstacck-developer position.
+//     </p>
+//   </div>
+
+//   <Media/>
+//   </div>
+// }
+
 
 function App() {
   return (
@@ -16,10 +32,11 @@ function App() {
       <Router>
         <Nav/>
         <Switch>
-        <Route exact path='/' component={Intro} />
         <Route exact path='/about' component={About} />
         <Route exact path='/portfolio' component={Portfolio} />
         <Route exact path='/contact' component={Contact} />
+        {/* The default route */}
+        <Route path='*' component={Intro} />
         </Switch>
       </Router>
     </div>
